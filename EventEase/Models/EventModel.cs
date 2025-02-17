a9fae491dab6b77ff4cc5741aa493b8868530f3e
Andy Kramer <andykramer3@gmail.com>
2025-02-17 15:24:51 -0500

    Initial commit

---
+++ EventEase/Models/EventModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EventEase.Models
+{
+    public class EventModel
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
